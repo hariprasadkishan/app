59c58eba17d3bbbb4da54a6ec6256e5797c006e6
Alex McDominey <sugyanworkplace@gmail.com>
2026-05-14 23:49:00 +0530

    Added folders for Frontend and Backend with minimal backend setup

---
+++ Backend/src/index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
