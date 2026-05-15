9197e9b9d8f69cdfcea686c5a05c59828e33d613
Alex <sugyanworkplace@gmail.com>
2026-05-16 00:19:32 +0530

    Dev (#1) set up mongoDB and Cloudinary
    * Connected Mongo database
    
    * Set up Cloudinary, CLOUDINARY_CLOUD_NAME=dkmfxp2ho

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
-import app from "./app.js";
+import connectDB from "./db/index.js";
+import app from './app.js'
 
 dotenv.config({
-    path: "./.env"
-});
+    path: './.env'
+})
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+const port = process.env.PORT || 8000
+
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running at port ${port}`)
+    })
+})
+.catch((error) => {
+    console.log(`MONGODB connection FAILED !!!`, error)
+})
